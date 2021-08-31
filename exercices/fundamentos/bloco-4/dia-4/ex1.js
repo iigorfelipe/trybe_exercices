345602477e43c1aff87cdafef3d75d6d86a8042f
Igor <iigorfelipe@gmail.com>
2021-08-31 11:59:28 -0300

    resolve alguns exercicios de function

---
+++ exercices/fundamentos/bloco-4/dia-4/ex1.js
@@ -0,0 +1,9 @@
+//  Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+function palidromeOrNo() {
+  if (asa === asa.split('').reverse().join('')){
+    return true
+  }
+}
+let asa = 'asa'
+palidromeOrNo()

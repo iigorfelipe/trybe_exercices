108b45bbb634b37e720d38982e1e10de73f80df8
Igor <iigorfelipe@gmail.com>
2021-09-27 19:54:19 -0300

    hehe

--- exercices/fundamentos/bloco-7/dia-2/dia-2.2/script.js
+++ exercices/fundamentos/bloco-7/dia-2/dia-2.2/script.js
@@ -38,14 +38,19 @@ const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
   return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}.`
 
-  // Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+  // "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
+  const pizzas = Object.keys(order.order.pizza);
+  const drink = order.order.drinks.coke.type;
+  const total = order.order.drinks.coke.price + order.order.pizza.marguerita.price + order.order.pizza.pepperoni.price;
+  console.log(`Olá Luiz Silva, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drink} é R$ ${total},00.`)
 
+  // "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
 }
 
 orderModifier(order);

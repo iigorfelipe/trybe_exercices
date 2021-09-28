957d82e9819b5f2a097873d630b790c487f2c00e
Igor <iigorfelipe@gmail.com>
2021-09-28 09:31:03 -0300

    finaliza parte 1 e completa 6 exercicios da parte 2

--- exercices/fundamentos/bloco-7/dia-2/dia-2.2/script2.js
+++ exercices/fundamentos/bloco-7/dia-2/dia-2.2/script2.js
@@ -18,17 +18,31 @@ const lesson3 = {
   turno: 'noite',
 };
 
+// 1)
 const addTurno = (l, j, ll) => {
   l[j] = ll;
 }
- 
 addTurno(lesson2, 'turno', 'noite');
 
+// 2)
 const allKeys = obj => Object.keys(obj);
-console.log(allKeys(lesson1));
+// console.log(allKeys(lesson1));
 
+// 3)
 const objLength = ulala => Object.keys.length;
-console.log(objLength(lesson3));
+// console.log(objLength(lesson3));
 
+// 4)
 const allValues = iraaa => Object.values(iraaa);
-console.log(allValues(lesson3));
+// console.log(allValues(lesson3));
+
+// 5)
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+// console.log(allLessons);
+
+// 6)
+const students1 = allLessons.lesson1.numeroEstudantes;
+const students2 = allLessons.lesson2.numeroEstudantes;
+const students3 = allLessons.lesson3.numeroEstudantes;
+const returnTotal = allLessons => students1 + students2 + students3;
+console.log(returnTotal(allLessons));

48b524603dbb1adcefb1b8f8f1e22c4efdacefcc
Igor <iigorfelipe@gmail.com>
2021-10-11 10:32:44 -0300

    finaliza 'vamos praticar'

--- exercices/fundamentos/bloco-8/dia-5/script3.js
+++ exercices/fundamentos/bloco-8/dia-5/script3.js
@@ -11,7 +11,21 @@ const otherPerson = {
   nationality: 'Russian',
 };
 
-console.log(getNationality(otherPerson)); // Ivan is Russian
-console.log(getNationality(person));
+// console.log(getNationality(otherPerson)); // Ivan is Russian
+// console.log(getNationality(person));
 
 // Do jeito que está, quando passamos person para a função GetNationality o retorno é João is undefined . Ajuste a função GetNationality para que a chamada GetNationality(person) retorne João is Brazilian .
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
+
+// Agora é hora de praticar: altere a função getPosition utilizando a property shorthand .
+
+const multiply = (number, value = 1) => number * value;
+console.log(multiply(8));
+
+// Para praticar, escreva uma função multiply que multiplique dois números passados como argumentos. Atribua como default o valor 1 caso não seja passado nenhum valor como segundo parâmetro.

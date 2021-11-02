e33cdffada76fa7c598c0075002b11b97c783c21
Igor <iigorfelipe@gmail.com>
2021-11-02 08:43:29 -0300

    test bottons

--- exercices/02_front-end/bloco-11/dia-1/buttons/src/App.js
+++ exercices/02_front-end/bloco-11/dia-1/buttons/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function App() {
-  return (<span>test</span>);
+const botao1 = () => console.log('Clicou no botão 1');
+const botao2 = () => console.log('Clicou no botão 2');
+const botao3 = () => console.log('Clicou no botão 3');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={botao1}>Botão 1</button>
+        <button onClick={botao2}>Botão 2</button>
+        <button onClick={botao3}>Botão 3</button>
+      </div>
+    );
+  }
 }
 
 export default App;

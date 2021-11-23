ab4713a8ea38c1786269e181996b3d1eded991ec
Igor <iigorfelipe@gmail.com>
2021-11-23 11:33:38 -0300

    pokedex-router

---
+++ exercices/02_front-end/bloco-12/dia-2/pokedex-react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import pokemons from './data';
+import Pokedex from './Pokedex';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Route exact path='/' render={(props) => <Pokedex {...props} pokemons={pokemons} />}  />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

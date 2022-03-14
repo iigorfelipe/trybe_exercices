4188170b202604375d3d2b5ba5e71cfc4c2bb063
Igor <iigorfelipe@gmail.com>
2022-03-14 12:58:05 -0300

    prommiseAll

--- exercices/03_back-end/bloco-22/dia-2/io-local/writeFile.js
+++ exercices/03_back-end/bloco-22/dia-2/io-local/writeFile.js
@@ -1,9 +1,12 @@
 const fs = require('fs').promises;
 
-fs.writeFile('./meu-arquivo.txt', 'Meu textão')
-  .then(() => {
+async function main() {
+  try {
+    await fs.writeFile('./meu-arquivo.txt', 'Meu textão');
     console.log('Arquivo escrito com sucesso!');
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error(`Erro ao escrever o arquivo: ${err.message}`);
-  });
+  }
+}
+
+main()

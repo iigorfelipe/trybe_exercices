8b89822fd130ba12632b8214228663f7c0cdadea
Igor <iigorfelipe@gmail.com>
2022-03-15 11:17:18 -0300

    adidiona mais cenarios de teste

--- exercices/03_back-end/bloco-22/dia-3/tests/teste-media.js
+++ exercices/03_back-end/bloco-22/dia-3/tests/teste-media.js
@@ -9,3 +9,19 @@ describe('Quando a média for menor que 7', () => {
     expect(resposta).to.be.equals('reprovado');
   });
 });
+
+describe('Quando a média for maior que 7', () => {
+  it('retorna "aprovado"', () => {
+    const resposta = calculaMedia(9);
+
+    expect(resposta).to.be.equals('aprovado');
+  });
+});
+
+describe('Quando a média for igual a 7', () => {
+  it('retorna "aprovado"', () => {
+    const resposta = calculaMedia(7);
+
+    expect(resposta).to.be.equals('aprovado');
+  });
+});

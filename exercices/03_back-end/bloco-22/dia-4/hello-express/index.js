c0de63dae73aca3405fbfe65914ee048322f8ffa
Igor <iigorfelipe@gmail.com>
2022-03-16 12:52:46 -0300

    cria recipes

--- exercices/03_back-end/bloco-22/dia-4/hello-express/index.js
+++ exercices/03_back-end/bloco-22/dia-4/hello-express/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const app = express();
 
-const app = express(); // 1
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
 
-app.get('/hello', handleHelloWorldRequest); // 2
+app.get('/recipes', function (req, res) {
+  res.json(recipes);
+});
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
-}); // 3
-
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Olá mundo!');
-}
+});

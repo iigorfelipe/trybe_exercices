0f8d51d3ee4e0393a3fedbb221bd2a5b679e85f6
Igor <iigorfelipe@gmail.com>
2022-05-11 13:00:53 -0300

    instancia books para main

--- exercices/03_back-end/bloco-26/dia-2/model_mysql/main.ts
+++ exercices/03_back-end/bloco-26/dia-2/model_mysql/main.ts
@@ -1,9 +1,15 @@
 import connection  from './models/connection';
+import BookModel from "./models/Books";
 
 const main = async () => {
   const result = await connection.execute('SELECT * FROM books');
   const [rows] = result;
+
+  const bookModel = new BookModel(connection);
+  const books = await bookModel.getAll();
+
   console.log(rows);
+  console.log(books)
 }
 
 main();

c8d0ff71dee1a77244a37359ec871beb650fd93b
Igor <iigorfelipe@gmail.com>
2022-08-01 17:59:51 -0300

    feat: aprendendo python

---
+++ exercices/04_ciência_da_computação/bloco-33/dia-1/test.py
@@ -0,0 +1,20 @@
+def analyze(text):
+  count_words = len(text.split(" "))
+
+  return f"Número de palavras = {count_words}"
+
+text_to_analyze = (
+  "gomu gomu no... BAZOOKA!"
+)
+
+print(analyze(text_to_analyze))
+
+
+lista_dois = ["dois", "two", "dos", "2"]
+
+lista_dois.sort()
+
+# testando um erro
+
+for elemento in lista_dois:
+  print(int(elemento))
